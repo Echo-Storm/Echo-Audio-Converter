85864786c4a227d8b13658b6a553db5750ec2175
xechostormx <xechostormx@gmail.com>
2026-04-16 18:01:11 -0400

    A

--- ui/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Echo Audio Converter - UI module.
-"""
-
-from .main_window import MainWindow
-from .workers import UpdateWorker, BatchWorker
-
-__all__ = ["MainWindow", "UpdateWorker", "BatchWorker"]
